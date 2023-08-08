2ea5fff90f5193c05c76fc7db5d74c92ce31dfb4
Timothy Attah <timothyattah733@gmail.com>
2023-08-08 01:42:49 +0100

    Ctreated auth controller for register users

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,8 +6,47 @@ const authControllers = {
   register: async (req, res) => {
     try {
       const { fullname, username, email, password, gender } = req.body;
-      console.log(req.body);
-      res.json({ msg: 'Registered!' });
+      let newUsername = username.toLowerCase().replace(/ /g, '');
+
+      const user_name = await Users.findOne({ username: newUsername });
+      if (user_name)
+        return res.status(400).json({ msg: 'This user name already exists.' });
+
+      const user_email = await Users.findOne({ email });
+      if (user_email)
+        return res.status(400).json({ msg: 'This email already exists.' });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: 'Password must be at least 6 characters long.' });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = await new Users({
+        fullname,
+        username: newUsername,
+        email,
+        password: passwordHash,
+        gender,
+      });
+
+      const access_token = createAccessToken({ id: newUser._id });
+      const refresh_token = createRefreshToken({ id: newUser._id });
+
+      res.cookie('refreshtoken', refresh_token, {
+        httpOnly: true,
+        path: '/api/refresh_token',
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      });
+
+      await newUser.save();
+
+      res.json({
+        msg: 'Registered Successfully!',
+        access_token,
+        user: { ...newUser._doc, password: '' },
+      });
     } catch (err) {
       console.log(err);
       return res.status(500).json({ msg: err.message });
@@ -34,6 +73,17 @@ const authControllers = {
       return res.status(500).json({ msg: err.message });
     }
   },
+};
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '1d',
+  });
+};
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '30d',
+  });
 };
 
 module.exports = authControllers;
