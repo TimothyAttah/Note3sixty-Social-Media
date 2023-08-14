6aef90417c5d9b3fe1c88c6ea3a6c55928202e71
Timothy Attah <timothyattah733@gmail.com>
2023-08-14 05:59:29 +0100

    Created and dispatch register action to register.jsx file

--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -3,27 +3,28 @@ import { postDataAPI } from '../../utils/fetchData';
 import valid from '../../utils/valid';
 
 export const register = (data) => async (dispatch) => {
+  const check = valid(data);
+  if (check.errLength > 0)
+    return dispatch({ type: GLOBALTYPES.ALERT, payload: check.errMsg });
+
   try {
-    const check = valid(data);
-    if (check.errLength > 0)
-      return dispatch({ type: GLOBALTYPES.ALERT, payload: check.errMsg });
     dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } });
     const res = await postDataAPI('register', data);
     console.log(res);
-    // dispatch({
-    //   type: GLOBALTYPES.AUTH,
-    //   payload: {
-    //     token: res.data.access_token,
-    //     user: res.data.user,
-    //   },
-    // });
-    // localStorage.setItem('firstLogin', true);
-    // dispatch({
-    //   type: GLOBALTYPES.ALERT,
-    //   payload: {
-    //     success: res.data.msg,
-    //   },
-    // });
+    dispatch({
+      type: GLOBALTYPES.AUTH,
+      payload: {
+        token: res.data.access_token,
+        user: res.data.user,
+      },
+    });
+    localStorage.setItem('firstLogin', true);
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: {
+        success: res.data.msg,
+      },
+    });
   } catch (err) {
     dispatch({
       type: GLOBALTYPES.ALERT,
