16c80ee9264c4113fe2c402365e055eed74ab483
Timothy Attah <timothyattah733@gmail.com>
2023-09-24 22:11:37 +0100

    Created search feature both frontend and backend

--- client/src/components/header/Search.jsx
+++ client/src/components/header/Search.jsx
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getDataAPI } from '../../utils/fetchData';
+import { GLOBALTYPES } from '../../redux/types';
+import { UserCard } from '../UserCard';
 
 export const Search = () => {
   const [search, setSearch] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    setUsers([]);
+    setSearch('');
+  };
+
+  useEffect(() => {
+    if (search && auth.token) {
+      getDataAPI(`search?username=${search}`, auth.token)
+        .then((res) => setUsers(res.data.users))
+        .catch((err) => {
+          dispatch({
+            type: GLOBALTYPES.ALERT,
+            payload: { error: err.response.data.msg },
+          });
+        });
+    } else {
+      setUsers([]);
+    }
+  }, [search, auth.token, dispatch]);
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -23,7 +52,26 @@ export const Search = () => {
         <span>Search</span>
       </div>
 
-      {/* <div className='close_search'>&times;</div> */}
+      <div
+        className='close_search'
+        style={{ opacity: users.length === 0 ? 0 : 1 }}
+        onClick={handleClose}
+      >
+        &times;
+      </div>
+
+      <div className='users'>
+        {search &&
+          users.map((user) => (
+            <Link
+              key={user._id}
+              to={`/profile/${user._id}`}
+              onClick={handleClose}
+            >
+              <UserCard user={user} border='border' />
+            </Link>
+          ))}
+      </div>
     </form>
   );
 };
